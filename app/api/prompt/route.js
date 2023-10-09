ffe99fb7172e4fa0efe45aa5f07379cec7324085
srb1998 <svgupta@mitaoe.ac.in>
2023-10-09 19:30:50 +0530

    Fixed Feed Glitch

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -1,7 +1,7 @@
 import { connectToDB } from "@utils/database";
 import Prompt from "@models/prompt";
 
-export const revalidate = 1; //revalidate api every 1 second
+export const dynamic = 'force-dynamic';
 export const GET = async(request) => {
     try {
         await connectToDB();
